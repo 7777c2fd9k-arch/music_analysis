641a698fefca6afbd28b846aa64aa736e74d8354
7777c2fd9k-arch <miyapon082612010602@icloud.com>
2026-05-30 17:05:59 +0900

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.MUSIC_ANALYSIS_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://oqamtyllfzlevgnoqbic.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xYW10eWxsZnpsZXZnbm9xYmljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMTM3NDMsImV4cCI6MjA5NTY4OTc0M30.zeIWjGqXOYFTrpxNbL48KF6Gfr-mqQRB7f2qZl0iA7A",
 };
